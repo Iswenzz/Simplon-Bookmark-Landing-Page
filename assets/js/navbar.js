f7cd1464c87d99181314053015fa931316836671
iswenzz <suxlolz1528@gmail.com>
2020-06-25 19:19:56 +0200

    Navs: Added anchors and smooth scrolling & delayed redirection for modal anchors

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,11 +1,20 @@
 'use strict';
 
 /**
- * Navbar 
+ * Navbar events.
+ * - show/hide regular navbar when modal open/close
+ * - delay anchor redirection for the smooth scrolling
  */
 $('load', () =>
 {
 	let navbar = $('#main-navbar');
+
 	$('#navbarToggler').on('show.bs.modal', () => navbar.css("visibility", "hidden"));
 	$('#navbarToggler').on('hide.bs.modal', () => navbar.css("visibility", "visible"));
+
+	$('#navbarToggler a').click(function(e) 
+	{
+		e.preventDefault(); // prevent anchor redirection
+		setTimeout(() => window.location.href = this.href, 500);
+	});
 }, false);
